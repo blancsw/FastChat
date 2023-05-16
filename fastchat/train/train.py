b7c51b57922347e247d88ef40f31b1b92f3decd3
SwanBlanc <swan.blanc@infomaniak.com>
2023-05-16 11:10:08 +0200

    Edit dummy json, and train script

--- fastchat/train/train.py
+++ fastchat/train/train.py
@@ -79,7 +79,7 @@ def preprocess(
     tokenizer: transformers.PreTrainedTokenizer,
 ) -> Dict:
     conv = get_conversation_template("vicuna")
-    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+    roles = {"client": conv.roles[0], "support": conv.roles[1]}
 
     # Apply prompt templates
     conversations = []
